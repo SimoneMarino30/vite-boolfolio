d4c59dfb4e1253b81686a9cc03c8a6da3ca1c4e4
Simone Marino <simo.marino@outlook.it>
2023-04-24 21:20:17 +0200

    Axios call works + AppMain/AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+// AXIOS CALL
+import axios from "axios";
+// COMPONENTS
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 
 export default {
   data() {
     return {
-      title: "Hello world"
+      title: "Hello world",
+      projects: [],
     }
-  }
+  },
 
-  // components: {
-  //   MyComponent,
-  // },
+  components: { AppHeader, AppMain },
+
+  methods: {
+    fetchList() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+      // CIO' CHE VIENE LETTO DALL'API
+      .then((response) => {
+        this.projects = response.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchList();
+  }
 };
 </script>
 
 <template>
+  <AppHeader/>
   <div class="container">
-     <h1>{{ title }}</h1>
+    <AppMain/>
   </div>
  
 </template>
