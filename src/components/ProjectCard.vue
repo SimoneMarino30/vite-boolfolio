1b31cf7dfcf120d63a8ededf824313ce79ff1a65
Simone Marino <simo.marino@outlook.it>
2023-04-25 20:20:36 +0200

    Images + Placeholder

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -25,10 +25,15 @@ export default {
 <template>
     <div class="card" style="width: 18rem;">
       <figure>
-          <!-- <img :src="project.link ? asset('storage/' . project.link) : 'https://www.frosinonecalcio.com/wp-content/uploads/bfi_thumb/default-placeholder-38gbdutk2nbrubtodg93tqlizprlhjpd1i4m8gzrsct8ss250.png' " alt="" class="img-fluid"> -->
+          <!-- <img :src="project.link ? url('storage/' . project.link) : 'https://www.frosinonecalcio.com/wp-content/uploads/bfi_thumb/default-placeholder-38gbdutk2nbrubtodg93tqlizprlhjpd1i4m8gzrsct8ss250.png' " alt="" class="img-fluid"> -->
 
-        
-          <img :src="project.link" alt="project-image">
+          <!-- <img :src="project.link" alt="project-image"> -->
+
+          <!-- ! SOLUZIONE FAI DA TE -->
+          <div>
+            <img v-if="project.link" :src="project.link" alt="project-image" />
+            <img v-else src='https://www.frosinonecalcio.com/wp-content/uploads/bfi_thumb/default-placeholder-38gbdutk2nbrubtodg93tqlizprlhjpd1i4m8gzrsct8ss250.png' alt="placeholder-image" class="img-fluid"/>
+          </div>
           
           <figcaption>{{ project.date }}</figcaption>
       </figure>
